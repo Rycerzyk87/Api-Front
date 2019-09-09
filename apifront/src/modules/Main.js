671eccffb924fd0fa4d592a62a6baa0a02e1c1c8
Adam Samulski <a.samulski@op.pl>
2019-09-09 21:22:12 +0200

    new opening

---
+++ apifront/src/modules/Main.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import Graph from './Graph.js';
+
+
+
+
+
+
+class Main extends React.Component {
+    state = {
+        // domyślne wartości dla state, section wskazuje domyślną pozycję kalendarza
+        section: "cal",
+        year: 2019,
+        country: "PL",
+        data: [],
+        states: []
+    };
+
+    changeSemiSectionCal = (e) => {
+        this.setState({
+            section: "cal"
+        })
+    }
+    changeSemiSectionStat = (e) => {
+        this.setState({
+            section: "stat"
+        })
+    }
+    changeYearPlus = (e) => {
+        this.setState({
+            year: this.state.year + 1
+        })
+    }
+    changeYearMinus = (e) => {
+        this.setState({
+            year: this.state.year - 1,
+        })
+
+    }
+    handleDataFetchHolidays = (e) => {
+
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const targetUrl = `https://date.nager.at/Api/v1/Get/${this.state.country}/${this.state.year}`;
+
+        fetch(proxyUrl + targetUrl, {
+            method: 'GET',
+        }).then(response => response.json())
+            .then(data => {
+
+                this.setState({
+                    data: data
+                })
+            }
+            );
+    };
+
+    handleDataFetchStates = (e) => {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const targetUrl = 'https://date.nager.at/Api/v2/AvailableCountries';
+
+        fetch(proxyUrl + targetUrl, {
+            method: 'GET',
+        }).then(response => response.json())
+            .then(data => {
+                // console.log(data)
+                this.setState({
+                    states: data
+                })
+            }
+            );
+    };
+    componentDidMount() {
+        this.handleDataFetchHolidays()
+        this.handleDataFetchStates()
+
+    }
+    componentWillUpdate() {
+        this.handleDataFetchHolidays()
+    }
+    render() {
+        if (this.state.data.length === 0) {
+            return (
+                <div><h1>Oczekiwanie na dane...</h1></div>
+            );
+        }
+        return (
+            <div className="mainSection">
+
+                <div>
+                    <select onChange={(e) => this.setState({ country: e.target.value })}>
+
+                        {this.state.states.map((state) => <option key={state.key} value={state.key}>{state.value}</option>)}
+                    </select>
+                    <button onClick={this.changeSemiSectionCal}>Kalendarz</button>
+                    <button onClick={this.changeSemiSectionStat}>Statystyka</button>
+                </div>
+                <div>
+                    {/* na podstawie state zwracam odpowiednią sekcję */}
+                    {this.state.section === "cal" ? <div className="calender">
+                        <button onClick={this.changeYearPlus}>dodaj rok</button>
+                        <span>Wybrany rok to: {this.state.year}</span>
+                        <button onClick={this.changeYearMinus}>odejmij rok</button>
+                        <ul>
+                            {/* generuję z tablicy wszystkie święta */}
+                            {this.state.data.map((holiday) => <li key={holiday.date}>{holiday.date} {holiday.localName}</li>)}
+                        </ul>
+                    </div> :
+                        <div>
+                            <Graph />
+                        </div>}
+                </div>
+
+            </div >
+        );
+    }
+}
+
+export default Main;
